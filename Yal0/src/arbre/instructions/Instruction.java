8fbff5e1ffa58acc59ca36d98776c4ef42cc614d
Thelias <mercuryelements@gmail.com>
2019-01-14 13:35:50 +0100

    Commit initial : Création repo

---
+++ Yal0/src/arbre/instructions/Instruction.java
@@ -0,0 +1,11 @@
+package arbre.instructions;
+
+import arbre.ArbreAbstrait;
+
+public abstract class Instruction extends ArbreAbstrait {
+
+    protected Instruction(int n) {
+        super(n);
+    }
+
+}
