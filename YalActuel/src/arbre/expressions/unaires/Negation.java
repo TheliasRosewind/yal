ae5b3bfc838f78ed42f96094b128d5378acd849c
Thelias <mercuryelements@gmail.com>
2019-02-12 14:50:51 +0100

    Commit 12/02/19 : Negation, non logique et parenthèses
     - Renommage d'Opposition en Négation + fix
    
     - Ajout du non logique
    
     - Ajout de l'utilisation de parenthèses

--- YalActuel/src/arbre/expressions/unaires/Negation.java
+++ YalActuel/src/arbre/expressions/unaires/Negation.java
@@ -1,14 +1,15 @@
-package arbre.expressions;
+package arbre.expressions.unaires;
 
+import arbre.expressions.Expression;
 import exceptions.NonConcordanceTypeException;
 import tds.types.Type;
 import tds.types.TypesVariable;
 
-public class Opposition extends Expression {
+public class Negation extends Expression {
 
     protected Expression exp;
 
-    public Opposition(Expression g, int n){
+    public Negation(Expression g, int n){
         super(n);
         this.exp=g;
     }
@@ -17,7 +18,7 @@ public class Opposition extends Expression {
     public void verifier() {
         exp.verifier();
         if(!exp.getType().concorde(new Type(TypesVariable.ENTIER))){
-            throw new NonConcordanceTypeException(noLigne,"Opposition nécessite un entier ");
+            throw new NonConcordanceTypeException(noLigne,"Negation nécessite un entier ");
         }
     }
 
@@ -32,7 +33,7 @@ public class Opposition extends Expression {
         sb.append("    # Début de l'opposée \n" +
                 exp.toMIPS() +
                 "     # Execution de l'opposée\n" +
-                "     subu $v0, $zero, $t8\n" +
+                "     negu $v0, $v0\n" +
                 "     #Fin de l'opposition\n");
         return sb.toString();
     }
