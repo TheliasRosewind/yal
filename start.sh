64d749f79264ec3af2a1802574fdb8fba3551ba0
dibos2u <ophelia.dibos@gmail.com>
2019-02-13 14:47:08 +0100

    Script bash

--- start.sh
+++ start.sh
@@ -2,8 +2,6 @@
 
 rm -r assets/MIPS/*
 
-find assets/ -name "*.mips" -type f -delete
-
 cd YalActuel/src/analyse/
 
 java -jar /opt/depot/projetToucan/java-cup-11a.jar -parser AnalyseurSyntaxique -symbols CodesLexicaux Grammaire.cup
@@ -15,9 +13,12 @@ cd ../../..
 ant -buildfile yal.xml
 
 for filename in `find assets/yal/ -name "*.yal" -type f`; do
+    echo " "
+    echo "$filename" | sed -e 's/^assets\/yal\///'
     java -cp out/production/YalActuel/:/opt/depot/projetToucan/java-cup-11a.jar:/opt/depot/projetToucan/jflex-1.6.1.jar Yal "$filename"
 done
 
-mkdir `find assets/yal/ -type d -name "*" | sed -e 's/^assets\/yal\/$//' | sed -e 's/yal\//MIPS\//g' | sed '/^$/d'`
-
-mv `find assets/yal/ -type f -name "*.mips" | sed -e 's/^.\///'` `find assets/yal/ -type f -name "*.mips" | sed -e 's/yal\//MIPS\//g' | sed -e 's/\/[a-zA-Z1-9]*.mips//'`
+for filename in `find assets/yal/ -name "*.mips" -type f`; do
+    mkdir -p `echo "$filename" | sed -e 's/yal\//MIPS\//g' | sed -e 's/\/[a-zA-Z1-9(-)]*.mips//'`
+    mv "$filename" `echo "$filename" | sed -e 's/yal\//MIPS\//g'`
+done
