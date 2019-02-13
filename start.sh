8f752145a85fc4b83d4c9604bfb2fc11f3a2d174
dibos2u <ophelia.dibos@gmail.com>
2019-02-13 13:54:30 +0100

    Script bash

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-rm assets/*.mips
+rm -r assets/MIPS/*
+
+find assets/ -name "*.mips" -type f -delete
 
 cd YalActuel/src/analyse/
 
@@ -12,6 +14,10 @@ cd ../../..
 
 ant -buildfile yal.xml
 
-for filename in assets/*.yal; do
+for filename in `find assets/yal/ -name "*.yal" -type f`; do
     java -cp out/production/YalActuel/:/opt/depot/projetToucan/java-cup-11a.jar:/opt/depot/projetToucan/jflex-1.6.1.jar Yal "$filename"
 done
+
+mkdir `find assets/yal/ -type d -name "*" | sed -e 's/^assets\/yal\/$//' | sed -e 's/yal\//MIPS\//g' | sed '/^$/d'`
+
+mv `find assets/yal/ -type f -name "*.mips" | sed -e 's/^.\///'` `find assets/yal/ -type f -name "*.mips" | sed -e 's/yal\//MIPS\//g' | sed -e 's/\/[a-zA-Z1-9]*.mips//'`
